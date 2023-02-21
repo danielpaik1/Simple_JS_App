fb1e67b059cd9e2b3f4beda03a7100c5f359f054
danielpaik1 <danielpaik1@gmail.com>
2023-02-20 23:21:45 -0500

    created new variable
    created new variable pokemonList and assigned to a blank array. Added three objects containing pokemons.

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+pokemonList = [
+    {name: "Bulbasaur", height: 7, types: ['grass', 'poison']},
+    {name: "Charmander", height: 7, types: ['fire', 'earth']},
+    {name: "Pikachu", height: 7, types: ['grass', 'electricity']}
+];
