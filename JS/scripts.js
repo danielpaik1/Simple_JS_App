4f8f91acd6ecfbe24dd31d51a02f87184f010977
danielpaik1 <danielpaik1@gmail.com>
2023-02-21 20:56:59 -0500

    1.3 added loops

--- JS/scripts.js
+++ JS/scripts.js
@@ -17,3 +17,16 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
+        if (pokemonList[i].height >= .7 && pokemonList[i].height > .5) {
+         console.log(pokemonList[i].name + "(height:" + pokemonList[i].height + ")." + "Wow! It's BIG pokemon!");
+         document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ")." + "Wow! It's BIG pokemon!" + "<br>");
+       } 
+       else if (pokemonList[i].height <= .5 && pokemonList[i].height > .3) {
+          console.log(pokemonList[i].name + "(height:" + pokemonList[i].height + ")." + "It's an average pokemon.");   
+          document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ")." + "It's an average pokemon."+ "<br>");   
+       } 
+       else {
+         console.log(pokemonList[i].name + "(height:" + pokemonList[i].height + ")." + "It's a small pokemon.");
+         document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + ")." + "It's a small pokemon.");
+       }
+}
