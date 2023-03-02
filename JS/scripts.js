cebeaea88a11718fe6426815bf6b9b93b2b54419
danielpaik1 <danielpaik1@gmail.com>
2023-03-01 23:44:30 -0500

    edit and rewrote foreach. Added IIFE

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,33 +1,48 @@
-let pokemonList = [
-    {
-        name: "Bulbasaur", 
-        height: .5, 
-        types: ['grass', 'poison']
-    },
-    {
-        name: "Charmander", 
-        height: .7, 
-        types: ['fire', 'earth']
-    },
-    {
-        name: "Pikachu", 
-        height: .3, 
-        types: ['grass', 'electricity']
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+            name: "Bulbasaur", 
+            height: .5, 
+            types: ['grass', 'poison']
+        },
+        {
+            name: "Charmander", 
+            height: .7, 
+            types: ['fire', 'earth']
+        },
+        {
+            name: "Pikachu", 
+            height: .3, 
+            types: ['grass', 'electricity']
+        }
+    ];// objects within arrays: list of pokemons
+    
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];// objects within arrays: list of pokemons
 
-pokemonList.forEach(function(pokemon){
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+}) ();
+
+function printArrayDetails(pokemon){
     if (pokemon.height >= .7 && pokemon.height > .5) {
         console.log(pokemon.name + "(height:" + pokemon.height + ")." + "Wow! It's BIG pokemon!");
         document.write(pokemon.name + "(height:" + pokemon.height + ")." + "Wow! It's BIG pokemon!" + "<br>");
       } 
-      else if (pokemon.height <= .5 && pokemon.height > .3) {
+      else if(pokemon.height <= .5 && pokemon.height > .3) {
          console.log(pokemon.name + "(height:" + pokemon.height + ")." + "It's an average pokemon.");   
-         document.write(pokemon.name + "(height:" + pokemon.height + ")." + "It's an average pokemon."+ "<br>");   
+         // document.write(pokemon.name + "(height:" + pokemon.height + ")." + "It's an average pokemon."+ "<br>");   
       } 
       else {
         console.log(pokemon.name + "(height:" + pokemon.height + ")." + "It's a small pokemon.");
         document.write(pokemon.name + "(height:" + pokemon.height + ")." + "It's a small pokemon.");
       }
-
-});
+};
+pokemonRepository.getAll().forEach(printArrayDetails);
