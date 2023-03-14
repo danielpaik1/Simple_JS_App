1a70443cbaa36c586860ee239e3c1b97a4940d89
danielpaik1 <danielpaik1@gmail.com>
2023-03-14 15:18:31 -0400

    loadList and loadDetails

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 let pokemonRepository = (function () {
-    let repository = [];// objects within arrays: list of pokemons
+    let repository = [];// objects within arrays: list of pokemons. erased because we are going to push the list from link  
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
@@ -17,11 +17,18 @@ let pokemonRepository = (function () {
       button.classList.add("button-class");
       listpokemon.appendChild(button);
       pokemonList.appendChild(listpokemon);
-      button.addEventListener('click', function(pokemon) {
+      button.addEventListener('click', function(event) {
         showDetails(pokemon);
       });
     }
-    
+    // 1st function added.
+    //its a promise.fetch function
+    //1. fetches the url defined on line 3 (promise)
+    //result will be "response"   
+    //returns the website's json
+    //then will run the json on forEach 
+    //then displays name and detailsUrl found in json.result
+    //item is ex. [picachu's details]
     function loadList() {
         return fetch(apiUrl).then(function (response) {
           return response.json();
@@ -31,13 +38,16 @@ let pokemonRepository = (function () {
               name: item.name,
               detailsUrl: item.url
             };
-            add(pokemon);
+            add(pokemon);  //this function is from line 5
           });
-        }).catch(function (e) {
+        }).catch(function (e) { //catches if any error 
           console.error(e);
         })
       }
-
+      //4. loadDetails
+      //take para called item 
+      //detailsUrl is coming from line 39 
+      //item is details eventually 
     function loadDetails(item) {
         let url = item.detailsUrl;
         console.log(item.detailsUrl)
@@ -52,25 +62,26 @@ let pokemonRepository = (function () {
           console.error(e);
         });
       }
-
-    function showDetails(item) {
-        loadDetails(item).then(function(){
-            console.log(item);
+      //5. executes loadDetails and pass pokemon as argument or parameter
+      //then whatever from this gets passed into the console.log 
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function(){
+            console.log(pokemon);
         });
     }
 
     return {
         add: add,
         getAll: getAll,
-        addListItem: addListItem,
+        addListItem: addListItem, //2. add loadList 
         loadList: loadList,
         loadDetails: loadDetails,
         showDetails: showDetails
         };
 })();
-
-pokemonRepository.loadList().then(function() {
-
+//3.pass all pokemon so it will return all pokemon
+pokemonRepository.loadList().then(function() { 
+//wil pass these two functions below 
 pokemonRepository.getAll().forEach(function(pokemon){
   pokemonRepository.addListItem(pokemon);
     });
